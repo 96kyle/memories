0590ef9fcfcd3807c38253718b3f939cb422eb30
gwanhwikim <rhksgnl11@naver.com>
2023-08-07 14:28:05 +0900

    firebase연동

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:memories/firebase_options.dart';
 import 'package:memories/home_page.dart';
 import 'package:memories/utils/custom_color.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
